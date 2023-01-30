68bbceb96aaa29871fac18e76fcc95a3628f9739
reignkk1 <ruaals456@naver.com>
2023-01-30 22:23:56 +0900

    if & else

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-def plus(a,b):
-  print(a+b)
+my_age = 21
 
-def minus(a,b):
-  print(a-b)
-
-def multiplication(a,b):
-  print(a*b)
-
-def division(a,b):
-  print(a/b)
-
-def   square(a,b):
-  print(a**b)
-
-
-
-plus(123,634)
-minus(340,100)
-multiplication(3,4)
-division(4,2)
-square(2,2)
+if my_age > 20:
+  print('20살 초과입니다.')
+elif my_age <20:
+  print('20살 미만입니다.')
+else:
+  print('20살 입니다.')
