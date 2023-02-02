7f1fcedf5a75072e39762a7e86600c6bfbd9f0a4
reignkk1 <ruaals456@naver.com>
2023-02-02 23:29:06 +0900

    data

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-from random import randint
+player = {
+  'name':'김민겸',
+  'age':26,
+  'nickname':'잘생긴',
+  'favorite_food':['pizza','chicken']
+}
 
-
-print('카지노에 오신걸 환영합니다.')
-
-computer_number = randint(1,5)
-
-
-while True:
-
-  user_number = int(input('숫자를 입력해주세요!'))
-  if user_number == computer_number:
-    print('승리!')
-    break
-  elif user_number < computer_number:
-    print('작습니다 ㅠㅠ 실패')
-  else:
-    print('큽니다 ㅠㅠ 실패')
-
+print(player)
+player['favorite_food'].append('kimchi')
+print(player)
 
 
