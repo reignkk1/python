d52c1137365f82cac06f82d6e49f37f9bd625d0e
reignkk1 <ruaals456@naver.com>
2023-02-10 23:01:08 +0900

    soup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-base_url = 'https://weworkremotely.com/remote-jobs/search?=%E2%9C%93&term='
+base_url = 'https://weworkremotely.com/remote-jobs/search?term='
 search_term = 'python'
 
 response = get(f'{base_url}{search_term}')
@@ -9,5 +9,13 @@ response = get(f'{base_url}{search_term}')
 if response.status_code != 200:
   print('에러가 발생했습니다.')
 else:
-  soup = BeautifulSoup(response.text,'html.parser')
-  print(soup.find_all('title'))
+  soup = BeautifulSoup(response.text,"html.parser")
+  jobs = soup.find_all('section',class_="jobs")
+  for job_section in jobs:
+      job_posts = job_section.find_all('li')
+      job_posts.pop(-1)
+      for post in job_posts:
+       print(post)
+       print('//////////')
+  
+      
