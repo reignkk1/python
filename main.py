8b20b5d2889d6e825e4d531440d1c41825002156
reignkk1 <ruaals456@naver.com>
2023-02-13 22:09:07 +0900

    practice

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from requests import get
 from bs4 import BeautifulSoup
 
 base_url = 'https://weworkremotely.com/remote-jobs/search?term='
-search_term = 'python'
+search_term = 'java'
 
 response = get(f'{base_url}{search_term}')
 
 if response.status_code != 200:
   print('에러가 발생했습니다.')
 else:
+  results = []
+
   soup = BeautifulSoup(response.text,"html.parser")
   jobs = soup.find_all('section',class_="jobs")
+
   for job_section in jobs:
       job_posts = job_section.find_all('li')
       job_posts.pop(-1)
@@ -20,7 +23,17 @@ else:
         link = anchor['href']
         company,time,region = anchor.find_all('span',class_='company')
         title = anchor.find('span',class_='title')
-        print(company,time,region,title)
         
+        job_data = {
+          'company': company.string,
+          'region':region.string,
+          'time':time.string,
+          'title':title.string
+        }
+        results.append(job_data)
+  for result in results:
+    print(result)
+    print('//////////')   
+    
   
       
