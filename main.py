e17df2c871de57bd46a31117c85096cc23d25ac8
reignkk1 <ruaals456@naver.com>
2023-02-20 22:07:47 +0900

    repactor

--- main.py
+++ main.py
@@ -1,39 +1,8 @@
 from requests import get
 from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
 
-base_url = 'https://weworkremotely.com/remote-jobs/search?term='
-search_term = 'java'
-
-response = get(f'{base_url}{search_term}')
-
-if response.status_code != 200:
-  print('에러가 발생했습니다.')
-else:
-  results = []
-
-  soup = BeautifulSoup(response.text,"html.parser")
-  jobs = soup.find_all('section',class_="jobs")
-
-  for job_section in jobs:
-      job_posts = job_section.find_all('li')
-      job_posts.pop(-1)
-      for post in job_posts:
-        anchors = post.find_all('a')
-        anchor = anchors[1]
-        link = anchor['href']
-        company,time,region = anchor.find_all('span',class_='company')
-        title = anchor.find('span',class_='title')
-        
-        job_data = {
-          'company': company.string,
-          'region':region.string,
-          'time':time.string,
-          'title':title.string
-        }
-        results.append(job_data)
-  for result in results:
-    print(result)
-    print('//////////')   
-    
+jobs = extract_wwr_jobs('python')
+print(jobs)
   
       
