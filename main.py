b4a677d500002bfbb799110c4d6b479b025fecfb
reignkk1 <ruaals456@naver.com>
2023-02-27 22:54:17 +0900

    selenium

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from requests import get
+
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-jobs = extract_wwr_jobs('python')
-print(jobs)
+
+base_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
+
+options = Options()
+options.add_experimental_option("detach",True)
+
+browser = webdriver.Chrome(options=options)
+
+
+browser.get(f"{base_url}{search_term}")
+
+
+
   
       
